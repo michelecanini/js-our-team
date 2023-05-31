37c18d4b3df50e1c08d4c3a3815d7259378a4c9b
michelecanini <studio.michelecanini@gmail.com>
2023-05-31 16:56:55 +0200

    [UPDATE] insert variable array

--- js/script.js
+++ js/script.js
@@ -4,3 +4,15 @@
 // MILESTONE 0:Creare l’array di oggetti con le informazioni fornite (file informazioni team).
 //MILESTONE 1: Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
 //MILESTONE 2:Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+// DICHIARAZIONE DELLE VARIABILI
+
+// VARIABILE DELL'ARRAY MEMBRI DEL TEAM
+let teamMember = [
+    {
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        image: "wayne-barnett-founder-ceo.jpg"
+    }
+]
+ 
